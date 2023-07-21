1904d888a9dc7715ffcfc164838c6d7bb1b4686a
Carmel-Infanta <carmel@pearlthoughts.com>
2023-07-21 13:15:28 +0530

    playwright

--- tests/adminlogin.spec.js
+++ tests/adminlogin.spec.js
@@ -2,7 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://admin.1851dev.com/');
+  await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Username').fill('pearlthoughts');
+  await page.getByPlaceholder('Password').click();
   await page.getByPlaceholder('Password').fill('2v9uzHmjGEwShpP');
   await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.getByRole('heading', { name: 'User List' }).click();
 });
